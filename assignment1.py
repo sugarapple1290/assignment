d74c3b6a7f57f42ed6830b33a98d2fa7c83b9b84
sugarapple1290 <zeeshanhadiya4@gmail.com>
2025-08-24 05:59:28 -0700

    1

--- assignment1.py
+++ assignment1.py
@@ -1,3 +1,15 @@
-Student="hadiya"
+Student= "hadiya"
 print(Student)
 print("Student")
+School= "Emshighschool"
+print(School)
+print("School")
+Grade= "7"
+print(Grade)
+print("Grade")
+Ambition= "Cyber security analyst"
+print(Ambition)
+print("Ambition")
+Hobby= "Drawing"
+print(Hobby)
+print("Hobby")
