a1fd812c22599c3975c714ec525c8939c508e216
sugarapple1290 <zeeshanhadiya4@gmail.com>
2025-09-13 23:11:30 -0700

    1

--- assignment4.py
+++ assignment4.py
@@ -1,9 +1,41 @@
-a= '4.5869'
-print(a)
-print(type(a))
-b=int(a)
+# 1part
+
+a="true"
+b=bool(a)
 print(b)
 print(type(b))
 
-a='True'
+a="false"
+b=bool(a)
+print(b)
+print(type(b))
 
+a="0"
+b=bool(a)
+print(b)
+print(type(b))
+
+c="1"
+b=bool(a)
+print(b)
+print(type(b))
+
+
+# 2 part
+a='4.5769'
+b=float(a)
+c=int(b)
+print(type(c))
+
+#part c
+a=True
+b=float(a)
+print(b)
+
+a=False
+b=float(a)
+print(b)
+
+a='python'
+b=float(a)
+print(b)
