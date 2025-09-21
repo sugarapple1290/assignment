d6874d99b00b5a7a15aa625ddb016a6c6f86d941
sugarapple1290 <zeeshanhadiya4@gmail.com>
2025-09-20 22:16:52 -0700

    1

--- assignment5.py
+++ assignment5.py
@@ -1,6 +1,13 @@
+# A Users feedback abount product
+
 Name=input("What is your name")
 print(type(Name))
 Date=input("what is the date")
 print(type(Date))
 feedback=input("what is your feedback about my new product") 
 print(type(feedback)) 
+'''
+it shows the name of the user and also 
+the date he is giving the feedback on
+his feedback is on his own opinion
+'''
