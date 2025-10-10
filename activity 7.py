977764083668e205f51809a0cecd7651e3c78b48
sugarapple1290 <zeeshanhadiya4@gmail.com>
2025-10-09 22:01:15 -0700

    1

---
+++ activity 7.py
@@ -0,0 +1,15 @@
+#Area of a rectangle
+a=int(input("what is the length of the rectangle: "))
+b=int(input("what is the breath of the rectangle : "))
+print(4*12)
+
+# area of a square
+a=int(input('what is the length of the square: '))
+b=int(input("what is the breath of the rectangle; "))
+print(4*4)
+
+#area of a traingle
+a=int(input("what is the height of the triangle: "))
+b=int(input("what is the breath of the traingle: "))
+print(1/2*(6*3))
+
