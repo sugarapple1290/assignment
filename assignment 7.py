87d25b0e3cfede2022e32c1fbef5361c0fc007e3
sugarapple1290 <zeeshanhadiya4@gmail.com>
2025-10-10 01:47:46 -0700

    1

--- assignment 7.py
+++ assignment 7.py
@@ -1,24 +1,24 @@
 #Area of a rectangle
-'''
+
 
 a=int(input("what is the length of the rectangle: "))
 b=int(input("what is the breath of the rectangle : "))
-print(4*12)
+print(a*b)
 
 # area of a square
 a=int(input('what is the length of the square: '))
 b=int(input("what is the breath of the rectangle; "))
-print(4*4)
+print(b*a)
 
 #area of a traingle
 a=int(input("what is the height of the triangle: "))
 b=int(input("what is the breath of the traingle: "))
-print(1/2*(6*3))'''
+print(1/2*(a*b))
 
 #grocery list
-milk=int(input("what is the cost of the milk: "))
+'''milk=int(input("what is the cost of the milk: "))
 butter=int(input("what is the price of the butter: "))
 bread=int(input("what is the price of the bread: "))
 chocolate=int(input("what is the price of the chocolate: "))
 candy=int(input("what is the price of the candy: "))
-print(milk+butter+bread+chocolate+candy)
+print(milk+butter+bread+chocolate+candy)'''
