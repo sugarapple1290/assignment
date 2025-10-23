06795d3ce89912676c713ea6e4603e19ccce9d3b
sugarapple1290 <zeeshanhadiya4@gmail.com>
2025-10-23 03:22:23 -0700

    1

---
+++ assignment9.py
@@ -0,0 +1,111 @@
+#Task:1
+number = float(input ("Enter a number:"))
+if number >0:
+    print("The number is positive")
+
+ 
+
+# Task:2
+Number = int(input ("Enter a number:"))
+if number%2 == 0:
+    print(Number, "is an even number")
+
+
+
+
+# Task:3
+vote=int(input ("Enter your age:"))
+if vote>= 18:
+    print("You are eligible to vote")
+else:
+    print("You are'nt eligible to vote")
+ 
+
+
+ #Task:4
+temp =float(input  ("Enter the temperature  in °C:"))
+
+if temp > 30:
+    print("It is hot today")
+   
+
+# Task:5
+number = int(input("Enter a number:"))
+if number % 5== 0:
+    print("The number is divisible by 5")
+else:
+    print("The number is not divisible by 5")
+
+
+# Task:6
+score=int(input("Enter your marks:"))
+if score >50:
+    print("You passed")
+
+
+#Task:7
+username =input("Enter username:")
+if username == "Admin":
+    print(" correct username")
+else:
+    print(" wrong username")
+
+
+#Task:8
+number = float(input ("Enter a number:"))
+if number > 0:
+    print("The number is positive")
+else:
+    print("The number is not positive")
+
+#Task:9
+number = int(input ("Enter a number:"))
+if number % 2 == 0:
+    print(number, "is an even number")
+else:
+    print("The number is odd")
+
+#Task:10
+age= int(input("Enter your age:"))
+if age >= 18:
+    print("You are eligible to vote")
+else:
+    print("You arent eligible to vote")
+
+#Task:11
+Temp= float(input("Enter the temperature:"))
+if Temp >= 30:
+    print("It is hot")
+else:
+    print("It is cold")
+
+#Task:12
+number= int(input("Enter a number:"))
+if number % 3 == 0:
+    print("The number is divisible by 3")
+else:
+    print("The number is not divisible by 3")
+
+#Task:13
+Marks=int(input("Enter the students marks:"))
+if Marks >= 40:
+    print("pass")
+else:
+    print("fail")
+
+
+#Task:14
+
+Cpassword = "84"  
+Password = input("Enter the password:")
+if Password == Cpassword:
+    print("Password is correct")
+else:
+    print("Password is incorrect")
+
+#Task:15
+year = int(input("Enter a year:"))
+if year > 2000:
+    print("the year is Greater than 2000")
+else:
+    print("the year isnt Greater than 2000")
